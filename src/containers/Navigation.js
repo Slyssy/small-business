bf0b66993e1b71549ccf34613e620dd6a0ce1bc3
Stephen <stephen.lyssy@gmail.com>
2022-12-20 20:06:02 -0600

    Removed unused variables and changed path to component/navigation to reflect the fact I was no longer mapping to state or dispatch.

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -1,17 +1,17 @@
-import { connect } from 'react-redux';
-import Navigation from '../components/Navigation';
-import { openDialog } from '../redux/actions';
+// import { connect } from 'react-redux';
+// import Navigation from '../components/Navigation';
+// import { openDialog } from '../redux/actions';
 
-const mapStateToProps = (state) => {
-  return {
-    listings: state.listings,
-    dialogOpen: state.dialogOpen,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     listings: state.listings,
+//     dialogOpen: state.dialogOpen,
+//   };
+// };
 
 // const MapDispatchToProps = (dispatch) => {
 //   return {
 //     openDialog: () => dispatch(openDialog()),
 //   };
 // };
-export default connect(mapStateToProps, null)(Navigation);
+// export default connect(mapStateToProps, MapDispatchToProps)(Navigation);
